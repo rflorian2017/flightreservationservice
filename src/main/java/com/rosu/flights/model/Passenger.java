3678d02537cceef15c0cbc79baf4b1a759080d62
rflorian2017 <roby-l@DESKTOP-6V2BCMF>
2019-07-13 11:37:19 +0300

    Update Plane and Passenger

--- src/main/java/com/rosu/flights/model/Passenger.java
+++ src/main/java/com/rosu/flights/model/Passenger.java
@@ -1,10 +1,14 @@
 package com.rosu.flights.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "PASSENGER")
 public class Passenger {
 	
 	@Id
--- src/main/java/com/rosu/flights/model/Plane.java
+++ src/main/java/com/rosu/flights/model/Plane.java
@@ -1,10 +1,14 @@
 package com.rosu.flights.model;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "PLANE")
 public class Plane {
 
 	@Id
@@ -15,5 +19,32 @@ public class Plane {
 	@Column(name = "AIRCRAFT_MODEL")
 	private String aircraftModel;
 	
+	@Column(name = "SEAT_NO")
 	private int seatNo;
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getAircraftModel() {
+		return aircraftModel;
+	}
+
+	public void setAircraftModel(String aircraftModel) {
+		this.aircraftModel = aircraftModel;
+	}
+
+	public int getSeatNo() {
+		return seatNo;
+	}
+
+	public void setSeatNo(int seatNo) {
+		this.seatNo = seatNo;
+	}
+	
+	
 }
